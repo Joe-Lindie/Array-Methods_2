191429b7074b77d679470dcf07414e7a7027a519
Joe-Lindie <Josephlindie@hotmail.co.uk>
2022-05-01 14:30:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
-const userInput = document.getElementById("user_input");
-const evenNumsSelector = document.getElementById("even_nums_selector");
-const totalNumsSelector = document.getElementById("total_nums_selector");
-const button = document.getElementById("button");
-const result = document.getElementById("result");
+const userInput = document.getElementById("user_input")
+const evenNumsSelector = document.getElementById("even_nums_selector")
+const totalNumsSelector = document.getElementById("total_nums_selector")
+const button = document.getElementById("button")
+const result = document.getElementById("result")
 
 //Even Numbers Function
 const evenNumsFn = (arrOfNums) => {
-  const evenNumsResult = arrOfNums.filter((arrOfNums) => arrOfNums % 2 === 0);
-  result.textContent = evenNumsResult;
+  const evenNumsResult = arrOfNums.filter((arrOfNums) => arrOfNums % 2 === 0)
+  result.textContent = evenNumsResult
   //console.log(evenNumsResult)
-};
+}
 
 //Total Numbers Function
 const totalNumsFn = (arrOfNums) => {
-  const totalNumsResult = arrOfNums.reduce((total, amount) => total + amount);
+  const totalNumsResult = arrOfNums.reduce((total, amount) => total + amount)
 
   isNaN(totalNumsResult)
     ? (result.textContent = "Please enter numbers only.")
-    : (result.textContent = totalNumsResult);
+    : (result.textContent = totalNumsResult)
   //console.log(totalNumsResult)
   // returns true if value is NaN otherwise false
-};
+}
 
 button.addEventListener("click", function () {
-  const inputArr = userInput.value.split(",");
-  const arrOfNums = inputArr.map(Number);
+  const inputArr = userInput.value.split(",")
+  //console.log(inputArr)
+  const arrOfNums = inputArr.map(Number)
   //covert array of strings ['1',' 2',' 3'] to array of numbers [1,2,3]
   //console.log(arrOfNums)
 
@@ -33,8 +34,8 @@ button.addEventListener("click", function () {
     ? evenNumsFn(arrOfNums)
     : totalNumsSelector.checked
     ? totalNumsFn(arrOfNums)
-    : (result.textContent = `Please make a selection! Filter or reduce?`);
-});
+    : (result.textContent = `Please make a selection! Filter or reduce?`)
+})
 
 /*
 Add JavaScript so that when the user clicks the button, 
